a12a3733c44139dad6a88f3b46923f6d9c045250
RustyLesh <34222722+RustyLesh@users.noreply.github.com>
2022-04-25 16:15:30 +1200

    base item class scriptable object
    made a base item class

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    [SerializeField]string itemName;
+    [SerializeField]Texture2D sprite;
+}
